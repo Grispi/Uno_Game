bb89ac95ef05b8d4730c484dbd26951cb967d1e2
Grisel Alpanes <griselmelina@gmail.com>
2020-04-22 16:10:01 +0100

    Delete node_modules and .next

--- node_modules/next/dist/build/webpack/plugins/webpack-conformance-plugin/utils/ast-utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { types } from 'next/dist/compiled/recast';
-import { namedTypes } from 'ast-types';
-export declare function isNodeCreatingScriptElement(node: namedTypes.CallExpression): boolean;
-export declare function reducePropsToObject(propsNode: types.namedTypes.ObjectExpression): {};
--- node_modules/next/dist/lib/load-env-config.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Env = {
-    [key: string]: string;
-};
-export declare function loadEnvConfig(dir: string, dev?: boolean): Env | false;
