35c9380cbbe0e2476da4760c2249e463020ec5f9
Grisel Alpanes <griselmelina@gmail.com>
2020-05-07 22:30:06 +0100

    fix bug hidden cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ const defaultConfig = require("tailwindcss/defaultConfig");
 
 module.exports = {
   important: true,
-  theme: {
-    extend: {
-      height: {
-        "30": "7rem",
-      },
-    },
-  },
+  //   theme: {
+  //     extend: {
+  //       height: {
+  //         "30": "7rem",
+  //       },
+  //     },
+  //   },
   //   variants: {
   //     height: {
   //       "30": "7rem",
